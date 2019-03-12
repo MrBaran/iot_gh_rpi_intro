bbce850e370a97cd46f4b486a538989fc64f85d0
k2controls <kkelly@k2controls.net>
2019-03-12 08:30:22 -0400

    Create ghs_fan_control.py

---
+++ ghs_fan_control.py
@@ -0,0 +1,19 @@
+from time import sleep
+from iot_gh.IoTGreenhouseService import IoTGreenhouseService
+
+ghs = IoTGreenhouseService()
+
+print("IoT Greenhouse - Introduction to GPIO.")
+print("Control fan using IoT Greenhouse Service code")
+print()
+
+while True:
+    #switch_state = ghs.switches.toggle.get_state()
+    #print("state =", switch_state)
+    #switch_status = ghs.switches.toggle.get_status()
+    #print("status =", switch_status)
+    if switch_state.is_on():
+        ghs.fan.on()
+    else:
+        ghs.fan.off()
+    sleep(.5)
